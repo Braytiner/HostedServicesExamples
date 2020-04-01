9f2f016da500e98df34ecaca9715760053c6439a
Braytiner <braytiner@usersystem.com.br>
2020-04-01 18:39:53 -0300

    TimedBackgoundTasks

---
+++ TimedBackgroundTasks/Services/TimedHostedService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimedBackgroundTasks.Services
+{
+    public class TimedHostedService : IHostedService, IDisposable
+    {
+        private readonly ILogger<TimedHostedService> _logger;
+        private Timer _timer;
+
+        public TimedHostedService(ILogger<TimedHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Serviço TimedHosted iniciado!");
+            _timer = new Timer(FacaOTrabalho, null, TimeSpan.Zero,
+                TimeSpan.FromSeconds(5));
+
+            return Task.CompletedTask;
+        }
+
+        private void FacaOTrabalho(object state)
+        {
+            _logger.LogInformation(DateTime.Now.ToString());
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Serviço TimedHosted finalizado!");
+            _timer?.Change(Timeout.Infinite, 0);
+            
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
